ed9f8a73d46a95cad84882410ef26f194f567e22
Denis Solovyev <solovyev.denis@gmail.com>
2023-05-14 22:25:23 +0300

    ПР13: fix actions 2

--- controllers/users.js
+++ controllers/users.js
@@ -3,11 +3,10 @@ const User = require('../models/users');
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => {
-      res.send({ users });
+      res.send({ data: users });
     })
     .catch(() => {
-      res.status(500).send({ message: 'Ошибка' });
-      console.log('Ошибка поиска пользователей');
+      res.status(500).send({ message: 'Ошибка в работе сервера' });
     });
 };
 
@@ -15,31 +14,35 @@ const getUser = (req, res) => {
   const { userId } = req.params;
 
   User.findById(userId)
-    .orFail(() => {
-      throw new Error('Пользователь не найден');
-    })
-    .then((user) => {
-      res.send({ data: user });
-    })
-    .catch((e) => {
-      if (e.message === 'Пользователь не найден') {
-        res.status(400).send({ message: 'Пользователь не найден' });
-      } else {
-        res.status(500).send({ message: 'Ошибка' });
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(404).send({ message: 'Пользователь по указанному id не найден.' });
+        return;
       }
+      res.status(500).send({ message: 'Произошла ошибка в работе сервера.' });
     });
 };
 
-const createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
-    .then((newUser) => {
-      res.send(newUser);
+const updateUser = (req, res) => {
+  const owner = req.user._id;
+  const { name, about } = req.body;
+
+  User.findByIdAndUpdate(owner, { name, about })
+    .then((user) => {
+      res.send({ data: user });
+
+      if (!user) {
+        res.status(404D).send({
+          message: 'Пользователь c указанным id не найден.',
+        });
+        return;
+      }
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
         res.status(400).send({
-          message: 'Переданы некорректные данные при создании пользователя.',
+          message: 'Переданы некорректные данные при обновлении профиля.',
         });
         return;
       }
@@ -70,31 +73,35 @@ const updateUser = (req, res) => {
         });
         return;
       }
-      res
-        .status(500)
-        .send({ message: 'Ошибка' });
+      res.status(500).send({ message: 'Ошибка' });
     });
 };
 
 const updateAvatar = (req, res) => {
+  const owner = req.user._id;
   const { avatar } = req.body;
-  User.findByIdAndUpdate(
-    req.user._id,
-    { avatar },
-    { new: true },
-  )
+
+  User.findByIdAndUpdate(owner, { avatar })
     .then((user) => {
-      res.status(200).send(user);
-    })
-    .catch((evt) => {
-      if (evt.name === 'ValidationError') {
-        const message = Object.values(evt.errors)
-          .map((error) => error.message)
-          .join('; ');
-        res.status(400).send({ message });
-      } else {
-        res.status(500).send({ message: 'Ошибка' });
+      res.send({ data: user });
+
+      if (!user) {
+        res.status(404).send({
+          message: 'Пользователь c указанным id не найден.',
+        });
+        return;
       }
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({
+          message: 'Переданы некорректные данные при обновлении аватара.',
+        });
+        return;
+      }
+      res
+        .status(500)
+        .send({ message: 'Произошла ошибка в работе сервера.' });
     });
 };
 
